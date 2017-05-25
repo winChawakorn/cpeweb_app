e150962c3ac2947d4660489fd455f4104a5b9ff1
Pawan Intawongsa <pawan.i@ku.th>
2017-05-25 09:27:18 +0000

    Add 2 pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
 root 'static_pages#home'
 
-get 'static_pages/home'
 
-get 'static_pages/help'
+get '/help', to: 'static_pages#help'
 
-get 'static_pages/about'
+get '/about', to: 'static_pages#about'
+
+get '/admission', to: 'static_pages#admission'
 
 end
